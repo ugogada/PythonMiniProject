2199f06de171e8b203b38031ef8b1b2bcca2e587
ugogada <ugogada251@rku.ac.in>
2020-08-22 21:15:47 +0530

    create and connect to a database named “emp.db”

--- Project/connect.py
+++ Project/connect.py
@@ -3,6 +3,7 @@ class myconnect:
       
       def __init__(self):
             #4
+            self.con = sqlite3.connect("emp.db")
             #5      
                   
       def savetodb(self,ename,eemail,emob,etype,eexp,esalary):
