04c76970e1b77a28feaaee9f52a840bd82fe12fd
ugogada <ugogada251@rku.ac.in>
2020-08-22 21:09:25 +0530

    Implement “calculatesalary” method in PermanentEmployee.py

--- Project/PermanentEmployee.py
+++ Project/PermanentEmployee.py
@@ -3,6 +3,13 @@ class Per_Emp:
       basic_salary = 5000
       def calculatesalary(self,exp):
             #2
-                  
+            if (exp >= 15):
+                  return self.basic_salary + (self.basic_salary * 20) / 100
+            elif (10 <= exp < 15):
+                  return self.basic_salary + (self.basic_salary * 10) / 100
+            elif (5 <= exp < 10):
+                  return self.basic_salary + (self.basic_salary * 5) / 100
+            else:
+                  return self.basic_salary
                   
             
