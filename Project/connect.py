882480fb316a3e269a5083bf8eeb402c950bb10c
ugogada <ugogada251@rku.ac.in>
2020-08-22 21:17:59 +0530

    code to create a table named emp with appropriate columns and data types

--- Project/connect.py
+++ Project/connect.py
@@ -4,7 +4,16 @@ class myconnect:
       def __init__(self):
             #4
             self.con = sqlite3.connect("emp.db")
-            #5      
+            #5
+            self.con.execute(''' create table if not exists emp(
+                                        id integer primary  key AUTOINCREMENT,
+                                        name text,
+                                        email text,
+                                        mobile_no text,
+                                        type text,
+                                        experience integer,
+                                        salary text
+                                  ) ''')
                   
       def savetodb(self,ename,eemail,emob,etype,eexp,esalary):
             #6
